f558b57b6ffcfd133f14e44fa2c3e5ba70f2380b
Prem sai <126603806+Premsai8055@users.noreply.github.com>
2023-03-04 00:39:59 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1=input("Where do you want to go? left or right ?\n").lower()
+if choice1=="right":
+ print("You are running to lake.")
+ choice2=input("You reached to lake? What do want , swim or wait ?\n").lower()
+ if choice2=="wait":
+   print("You are running to monster temple.")
+   choice3=input("Select a colour to fight the monster, red , blue or yellow.\n").lower()
+   if choice3=="yellow":
+    print("You won the game.CONGRATULATIONS")
+   elif choice3=="red" or "blue":
+    print("You are attacked by snakes , GAME OVER.")
+ else:
+   print("Fall into a fire.Game over.")
+else:
+ print("Fall into a hole.Game over.")
+
+
+
